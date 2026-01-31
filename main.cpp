ce645e4d2354d763fe28d409ce52477e0e73934f
tenma <yakiimo1344@yahoo.co.jp>
2026-01-31 22:56:51 +0900

    ok

--- main.cpp
+++ main.cpp
@@ -1,86 +1,13 @@
-﻿#include<iostream>
-#include <list>
+﻿#include <iostream>
 #include <vector>
-
+#include <stdlib.h>
 using namespace std;
+
 
 int main()
 {
-	int const kNumStations = 30;
-	std::list<string> list{
-		"Osaki",
-		"Gotanda",
-		"Meguro",
-		"Ebisu",
-		"Shibuya",
-		"Harajuku",
-		"Yoyogi",
-		"Shinjuku",
-		"Shin - Ōkubo",
-		"Takadanobaba",
-		"Mejiro",
-		"Ikebukuro",
-		"Ōtsuka",
-		"Sugamo",
-		"Komagome",
-		"Tabata",
-
-		"Nippori",
-		"Uguisudani",
-		"Ueno",
-		"Okachimachi",
-		"Akihabara",
-		"Kanda",
-		"Tōkyo",
-		"Yurakucho",
-		"Shimbashi",
-		"Hamamatsuchō",
-		"Tamachi",
-
-		"Shinagawa",
-	};
+    printf_s("AAA");
 
 
-
-	int count = 0;
-	for (auto i = list.begin(); i != list.end(); ++i)
-	{
-		if (count == 15)
-		{
-			string name = "Nishi - Nippori";
-			i = list.insert(i, name);
-		}
-
-		else if (count == 27)
-		{
-			string name = "Takanawa Gateway";
-			i = list.insert(i, name);
-		}
-
-		count++;
-
-		
-
-
-	}
-
-
-	for (auto i = list.begin(); i != list.end(); ++i)
-	{
-
-		cout << (*i) << endl;
-
-	}
-
-
-
-
-
-
-
-	
-
-
-
-	return 0;
+    return 0;
 }
